d72bf667cc4f524adfdb8918c56ce9d32a25ee90
altiorainfotech-cell <altiorainfotech@gmail.com>
2026-03-24 02:50:33 +0530

    Config: allow images.unsplash.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['mongoose'],
   turbopack: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
